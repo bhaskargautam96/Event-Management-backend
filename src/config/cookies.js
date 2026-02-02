9d8c8ce285cea48fc5017a7091e787345b7cb55b
balancenutri <support@balancenutrition.com>
2026-02-02 11:59:53 +0530

    change in cok config

--- src/config/cookies.js
+++ src/config/cookies.js
@@ -1,17 +1,13 @@
-const isProd = process.env.NODE_ENV === "production";
-
 export const ACCESS_COOKIE_OPTIONS = {
-  httpOnly: true, // ALWAYS true
-  secure: isProd, // true in prod
-  sameSite: isProd ? "none" : "lax",
-  path: "/", // IMPORTANT
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: "lax",
   maxAge: 60 * 60 * 1000, // 1 hour
 };
 
 export const REFRESH_COOKIE_OPTIONS = {
-  httpOnly: true, // ALWAYS true
-  secure: isProd,
-  sameSite: isProd ? "none" : "lax",
-  path: "/", // IMPORTANT
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: "lax",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
