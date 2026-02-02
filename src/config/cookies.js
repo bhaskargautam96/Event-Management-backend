3239c4dba012cb6e2711651068d98c353d4d6121
balancenutri <support@balancenutrition.com>
2026-02-02 12:36:10 +0530

    just for testing

--- src/config/cookies.js
+++ src/config/cookies.js
@@ -3,16 +3,16 @@ const isProd = process.env.NODE_ENV === "production";
 
 export const ACCESS_COOKIE_OPTIONS = {
   httpOnly: true,
-  secure: isProd,
-  sameSite: isProd ? "none" : "lax",
-  path: "/", // 🔥 REQUIRED
+  secure: true,
+  sameSite: "none",
+  path: "/",
   maxAge: 60 * 60 * 1000,
 };
 
 export const REFRESH_COOKIE_OPTIONS = {
   httpOnly: true,
-  secure: isProd,
-  sameSite: isProd ? "none" : "lax",
-  path: "/", // 🔥 REQUIRED
+  secure: true,
+  sameSite: "none",
+  path: "/",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
